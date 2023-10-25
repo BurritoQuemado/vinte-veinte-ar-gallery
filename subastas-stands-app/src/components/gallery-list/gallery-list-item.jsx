a99eff10105b313d75392e071fb94b55155dac5f
Diego Flores <dfmonroy07@gmai.com>
2023-10-25 12:07:27 -0600

    feat: update hr height

--- subastas-stands-app/src/components/gallery-list/gallery-list-item.jsx
+++ subastas-stands-app/src/components/gallery-list/gallery-list-item.jsx
@@ -5,7 +5,7 @@ function ListItem({ item }) {
         
         <div>
             <Link to={item.href} target="_blank">
-                <div className="w-full overflow-hidden bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <div className="w-full overflow-hidden bg-gray-200 ">
                 <img
                     src={item.img}
                     alt={item.title}
@@ -15,7 +15,7 @@ function ListItem({ item }) {
                 <h3 className="mt-2 text-mid text-bold text-gray-700">{item.title}</h3>
                 <h3 className="mt-2 text-sm text-gray-700">{item.desc}</h3>
             </Link>
-            <hr className='mt-8 bg-gray-500 w-full  h-1 lg:h-4' />
+            <hr className='mt-8 bg-gray-500 w-full h-1 lg:h-1' />
         </div>
     )
 }
